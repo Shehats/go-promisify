a6b9a1601316a1f78338ecd61c199b8129438ecf
Sal <saleh.shehata@outlook.com>
2023-02-04 13:27:04 -0700

    Improvements: Clean up logic and pass through the arguments initially passed to the promisify functions.

--- promise/promise.go
+++ promise/promise.go
@@ -1,27 +1,18 @@
 package promise
 
-import (
-	"context"
-	"log"
-)
-
 // Promise interface that defines
 // methods that are similar to those
 // of Javascript
 type Promise[T any] interface {
-	Then(func(T) any) Promise[T]
-	ThenWithContext(func(T, *context.Context) any) Promise[T]
-	Catch(func(error) (any, error)) Promise[T]
-	CatchWithContext(func(error, *context.Context) (any, error)) Promise[T]
-	Finally(func(T, error) (any, error))
-	FinallyWithContext(func(T, error, *context.Context) (any, error))
+	Then(func(T, ...any) (any, error)) Promise[any]
+	Catch(func(error, ...any) (any, error)) Promise[any]
+	Finally(func(T, error, ...any))
 }
 
 type promiseImpl[T any] struct {
-	logger         *log.Logger
-	context        *context.Context
 	successChannel chan T
 	failureChannel chan error
+	argsChannel    chan []any
 }
 
 // Creates a new Promise instance
@@ -29,15 +20,17 @@ func newPromise[T any]() *promiseImpl[T] {
 	return &promiseImpl[T]{
 		successChannel: make(chan T, 1),
 		failureChannel: make(chan error, 1),
+		argsChannel:    make(chan []any, 1),
 	}
 }
 
 // PromisifyObject
 // Creates a promise from any type
 // Like JS's Promise.from(any)
-func PromisifyObject[T any](obj T) Promise[T] {
+func PromisifyObject[T any](obj T, args ...any) Promise[T] {
 	promise := newPromise[T]()
 	promise.successChannel <- obj
+	promise.argsChannel <- args
 	return promise
 }
 
@@ -48,12 +41,13 @@ func PromisifyObject[T any](obj T) Promise[T] {
 // If the function returns an object, puts the object in success channel
 func PromisifyFunc[T any](f func(...any) (T, error), args ...any) Promise[T] {
 	promise := newPromise[T]()
+	promise.argsChannel <- args
 	go func() {
 		obj, err := f(args)
 		if err != nil {
-			promise.successChannel <- obj
-		} else {
 			promise.failureChannel <- err
+		} else {
+			promise.successChannel <- obj
 		}
 	}()
 	return promise
@@ -62,48 +56,50 @@ func PromisifyFunc[T any](f func(...any) (T, error), args ...any) Promise[T] {
 // Then
 // Runs the then function using the
 // object in the success channel
-func (p *promiseImpl[T]) Then(successFunc func(T) any) Promise[T] {
-	return nil
-}
-
-// ThenWithContext
-// Runs the then function using the
-// object in the success channel and
-// also gives access to the context
-// in case this is used with database
-// connection or web application
-func (p *promiseImpl[T]) ThenWithContext(func(T, *context.Context) any) Promise[T] {
-	return nil
+func (p *promiseImpl[T]) Then(successFunc func(T, ...any) (any, error)) Promise[any] {
+	resultPromise := newPromise[any]()
+	args := <-p.argsChannel
+	resultPromise.argsChannel <- args
+	go func() {
+		arg := <-p.successChannel
+		obj, err := successFunc(arg, args...)
+		if err != nil {
+			resultPromise.failureChannel <- err
+		} else {
+			resultPromise.successChannel <- obj
+		}
+	}()
+	return resultPromise
 }
 
 // Catch
 // Runs the catch function using the
 // error in the error channel
-func (p *promiseImpl[T]) Catch(catchFunc func(error) (any, error)) Promise[T] {
-	return nil
-}
-
-// CatchWithContext
-// Runs the catch function using the
-// error in the error channel
-// also gives access to the context
-// in case this is used with database
-// connection or web application
-func (p *promiseImpl[T]) CatchWithContext(func(error, *context.Context) (any, error)) Promise[T] {
-	return nil
+func (p *promiseImpl[T]) Catch(catchFunc func(error, ...any) (any, error)) Promise[any] {
+	resultPromise := newPromise[any]()
+	args := <-p.argsChannel
+	resultPromise.argsChannel <- args
+	go func() {
+		err := <-p.failureChannel
+		obj, err := catchFunc(err, args...)
+		if err != nil {
+			resultPromise.failureChannel <- err
+		} else {
+			resultPromise.successChannel <- obj
+		}
+	}()
+	return resultPromise
 }
 
 // Finally
 // Runs the then function using the
 // object in the success channel
 // and the error in the error channel
-func (p *promiseImpl[T]) Finally(finallyFunc func(T, error) (any, error)) {}
-
-// FinallyWithContext
-// Runs the finally function using the
-// object in the success channel
-// and the error in the error channel
-// also gives access to the context
-// in case this is used with database
-// connection or web application
-func (p *promiseImpl[T]) FinallyWithContext(func(T, error, *context.Context) (any, error)) {}
+func (p *promiseImpl[T]) Finally(finallyFunc func(T, error, ...any)) {
+	go func() {
+		obj := <-p.successChannel
+		err := <-p.failureChannel
+		args := <-p.argsChannel
+		finallyFunc(obj, err, args...)
+	}()
+}
