6b61cf2334475da7b878621ae18dd441a7c532a3
Sal <saleh.shehata@outlook.com>
2023-02-10 13:49:14 -0700

    Bug Fix: Fix promise execution order and rename finally to do.

--- promise/promise.go
+++ promise/promise.go
@@ -1,36 +1,143 @@
 package promise
 
+import (
+	"reflect"
+	"sync"
+)
+
 // Promise interface that defines
 // methods that are similar to those
 // of Javascript
-type Promise[T any] interface {
-	Then(func(T, ...any) (any, error)) Promise[any]
-	Catch(func(error, ...any) (any, error)) Promise[any]
-	Finally(func(T, error, ...any))
+type Promise[T any] struct {
+	// successChannel is were the retun
+	// of the promise is stored
+	successChannel chan T
+	// failureChannel is were the error
+	// of the primise is stored
+	failureChannel chan error
+	// mutex that ensures that the promises
+	// are executed in order
+	mutex *sync.Mutex
+	// promise's wait group
+	wg *sync.WaitGroup
 }
 
-type promiseImpl[T any] struct {
-	successChannel chan T
-	failureChannel chan error
-	argsChannel    chan []any
+type meta struct {
+	objectType  reflect.Type
+	objectValue reflect.Value
 }
 
 // Creates a new Promise instance
-func newPromise[T any]() *promiseImpl[T] {
-	return &promiseImpl[T]{
+func newPromise[T any]() *Promise[T] {
+	return &Promise[T]{
 		successChannel: make(chan T, 1),
 		failureChannel: make(chan error, 1),
-		argsChannel:    make(chan []any, 1),
+		mutex:          &sync.Mutex{},
+		wg:             &sync.WaitGroup{},
 	}
 }
 
-// PromisifyObject
-// Creates a promise from any type
-// Like JS's Promise.from(any)
-func PromisifyObject[T any](obj T, args ...any) Promise[T] {
-	promise := newPromise[T]()
-	promise.successChannel <- obj
-	promise.argsChannel <- args
+func fromPromise[T, S any](promise *Promise[T]) *Promise[S] {
+	return &Promise[S]{
+		successChannel: make(chan S, 1),
+		failureChannel: make(chan error, 1),
+		mutex:          promise.mutex,
+		wg:             promise.wg,
+	}
+}
+
+func isFunction(obj any) bool {
+	return reflect.TypeOf(obj).Kind() == reflect.Func
+}
+
+// calls any function by reflection
+func call(obj any, args ...meta) (any, error) {
+	vargs := make([]reflect.Value, 0)
+	for _, arg := range args {
+		vargs = append(vargs, arg.objectValue)
+	}
+	function := reflect.ValueOf(obj)
+	ret := function.Call(vargs)
+	if !ret[1].IsNil() {
+		err := ret[1].Interface().(error)
+		return nil, err
+	}
+	return ret[0].Interface(), nil
+}
+
+func execute[T any](
+	promise *Promise[T],
+	f func(...any) (any, error),
+	args ...any) {
+	defer promise.mutex.Unlock()
+	defer promise.wg.Done()
+	obj, err := f(args...)
+	if err == nil {
+		promise.successChannel <- obj.(T)
+	} else {
+		promise.failureChannel <- err
+	}
+}
+
+func executeThenCallback[T, S any](
+	promise1 *Promise[T],
+	promise2 *Promise[S],
+	f func(T) (S, error),
+) {
+	defer promise2.mutex.Unlock()
+	defer promise2.wg.Done()
+	arg := <-promise1.successChannel
+	err := <-promise1.failureChannel
+	if err == nil {
+		obj, err := f(arg)
+		promise2.successChannel <- obj
+		promise2.failureChannel <- err
+	} else {
+		promise1.failureChannel <- err
+	}
+}
+
+func executeCatchCallback[T, S any](
+	promise1 *Promise[T],
+	promise2 *Promise[S],
+	f func(error) (S, error),
+) {
+	defer promise2.mutex.Unlock()
+	defer promise2.wg.Done()
+	err := <-promise1.failureChannel
+	if err != nil {
+		obj, err := f(err)
+		promise2.successChannel <- obj
+		promise2.failureChannel <- err
+	}
+}
+
+func executeFinallyCallback[T any](
+	promise *Promise[T],
+	f func(T, error),
+) {
+	defer promise.mutex.Unlock()
+	defer promise.wg.Done()
+	err := <-promise.failureChannel
+	obj := <-promise.successChannel
+	f(obj, err)
+}
+
+func Promisify[T any](obj any, args ...any) *Promise[T] {
+	var promise *Promise[T]
+	argsMeta := make([]meta, 0)
+	for _, arg := range args {
+		argsMeta = append(argsMeta, meta{
+			objectType:  reflect.TypeOf(arg),
+			objectValue: reflect.ValueOf(arg),
+		})
+	}
+	if isFunction(obj) {
+		runner := func(vargs ...any) (any, error) {
+			return call(obj, argsMeta...)
+		}
+		promise = promisifyFunc[T](runner, args)
+	}
 	return promise
 }
 
@@ -39,55 +146,33 @@ func PromisifyObject[T any](obj T, args ...any) Promise[T] {
 // from the function's result.
 // If the function returns an error, places the err in the failure channel
 // If the function returns an object, puts the object in success channel
-func PromisifyFunc[T any](f func(...any) (T, error), args ...any) Promise[T] {
+func promisifyFunc[T any](f func(...any) (any, error), args ...any) *Promise[T] {
 	promise := newPromise[T]()
-	promise.argsChannel <- args
-	go func() {
-		obj, err := f(args)
-		if err != nil {
-			promise.failureChannel <- err
-		} else {
-			promise.successChannel <- obj
-		}
-	}()
+	promise.wg.Add(1)
+	promise.mutex.Lock()
+	go execute(promise, f, args)
 	return promise
 }
 
 // Then
 // Runs the then function using the
 // object in the success channel
-func (p *promiseImpl[T]) Then(successFunc func(T, ...any) (any, error)) Promise[any] {
-	resultPromise := newPromise[any]()
-	args := <-p.argsChannel
-	resultPromise.argsChannel <- args
-	go func() {
-		arg := <-p.successChannel
-		obj, err := successFunc(arg, args...)
-		if err != nil {
-			resultPromise.failureChannel <- err
-		} else {
-			resultPromise.successChannel <- obj
-		}
-	}()
+func Then[T, S any](promise *Promise[T], successFunc func(T) (S, error)) *Promise[S] {
+	resultPromise := fromPromise[T, S](promise)
+	resultPromise.wg.Add(1)
+	promise.mutex.Lock()
+	go executeThenCallback(promise, resultPromise, successFunc)
 	return resultPromise
 }
 
 // Catch
 // Runs the catch function using the
 // error in the error channel
-func (p *promiseImpl[T]) Catch(catchFunc func(error, ...any) (any, error)) Promise[any] {
-	resultPromise := newPromise[any]()
-	args := <-p.argsChannel
-	resultPromise.argsChannel <- args
-	go func() {
-		err := <-p.failureChannel
-		obj, err := catchFunc(err, args...)
-		if err != nil {
-			resultPromise.failureChannel <- err
-		} else {
-			resultPromise.successChannel <- obj
-		}
-	}()
+func Catch[T, S any](promise *Promise[T], catchFunc func(error) (S, error)) *Promise[S] {
+	resultPromise := fromPromise[T, S](promise)
+	resultPromise.wg.Add(1)
+	promise.mutex.Lock()
+	go executeCatchCallback(promise, resultPromise, catchFunc)
 	return resultPromise
 }
 
@@ -95,11 +180,23 @@ func (p *promiseImpl[T]) Catch(catchFunc func(error, ...any) (any, error)) Promi
 // Runs the then function using the
 // object in the success channel
 // and the error in the error channel
-func (p *promiseImpl[T]) Finally(finallyFunc func(T, error, ...any)) {
-	go func() {
-		obj := <-p.successChannel
-		err := <-p.failureChannel
-		args := <-p.argsChannel
-		finallyFunc(obj, err, args...)
-	}()
+func (promise *Promise[T]) Do(finallyFunc func(T, error)) {
+	promise.wg.Add(1)
+	promise.mutex.Lock()
+	go executeFinallyCallback(promise, finallyFunc)
+	promise.wg.Wait()
+}
+
+// Exec
+// Waits for all of the promises to
+// execute
+func (promise *Promise[T]) Exec() {
+	promise.wg.Wait()
+}
+
+func (promise *Promise[T]) Await() (T, error) {
+	promise.wg.Wait()
+	obj := <-promise.successChannel
+	err := <-promise.failureChannel
+	return obj, err
 }
